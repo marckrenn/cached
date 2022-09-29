2eec3720c1469c5d735602ebe5b2c62fab26a30e
Marc Krenn <marc.krenn@gmail.com>
2022-09-29 23:24:02 +0200

    Initial commit

---
+++ cachedApp.swift
@@ -0,0 +1,17 @@
+//
+//  cachedApp.swift
+//  cached
+//
+//  Created by Marc Krenn on 29.09.22.
+//
+
+import SwiftUI
+
+@main
+struct cachedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
